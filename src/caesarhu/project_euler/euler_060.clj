56821e459e385115af997c17dc64f15cdd8eb761
caesarhu <hupeishun@gmail.com>
2022-01-21 16:55:29 +0800

    update euler-060

--- src/caesarhu/project_euler/euler_060.clj
+++ src/caesarhu/project_euler/euler_060.clj
@@ -1,9 +1,10 @@
 (ns caesarhu.project-euler.euler-060
   (:require [caesarhu.math.primes :as p]
-            [caesarhu.math.math-tools :as tools]))
+            [caesarhu.math.math-tools :as tools]
+            [clojure.core.memoize :as m]))
 
 (def concatenated-primes
-  (memoize
+  (m/lu
    (fn [n limit]
      (let [d (tools/digits n)]
        (set
@@ -11,13 +12,16 @@
               :when (and
                      (p/is-prime? (tools/digits->number (concat d (tools/digits a))))
                      (p/is-prime? (tools/digits->number (concat (tools/digits a) d))))]
-          a))))))
+          a))))
+   :lu/threshold 10000))
 
-(defn subset [limit & ks]
+(defn subset 
+  [limit & ks]
   (let [s (apply clojure.set/intersection (map #(concatenated-primes % limit) ks))]
     (apply (partial disj s) ks)))
 
-(defn solve [limit]
+(defn euler-060 
+  [limit]
   (first
    (for [a (p/primes-range 2 limit)
          b (subset limit a)
@@ -28,5 +32,19 @@
      {:sum (reduce + s) :primes s})))
 
 (comment
-  (time (solve 10000))
+  (time (euler-060 10000))
+  )
+
+(defn concatenated?
+  [p1 p2]
+  (let [d1 (tools/digits p1)
+        d2 (tools/digits p2)]
+    (->> [(concat d1 d2) (concat d2 d1)]
+         (map tools/digits->number)
+         (every? p/is-prime?))))
+
+(def limit-primes (p/primes-range 10000))
+
+(comment
+  (filter #(concatenated? 13 %) limit-primes)
   )
