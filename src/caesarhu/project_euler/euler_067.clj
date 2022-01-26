58181a99131964dbdb2e28e39fe929c5508560d5
caesarhu <hupeishun@gmail.com>
2022-01-26 18:53:53 +0800

    update euler-067

---
+++ src/caesarhu/project_euler/euler_067.clj
@@ -0,0 +1,14 @@
+(ns caesarhu.project-euler.euler-067
+  (:require [caesarhu.project-euler.euler-018 :refer [euler-018]]))
+
+(def fname "resources/p067_triangle.txt")
+
+(defn get-triangle []
+  (letfn [(parse-nums [s] (map #(Integer/parseInt %) (clojure.string/split s #"\W")))]
+    (->> (map parse-nums (clojure.string/split-lines (slurp fname)))
+         (map vec)
+         vec)))
+
+(comment
+  (time (euler-018 (get-triangle)))
+  )
