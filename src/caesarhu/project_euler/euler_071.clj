5042fd297953693e4d1df214addfc1db72c04972
caesarhu <hupeishun@gmail.com>
2022-01-28 06:43:00 +0800

    update euler-071

---
+++ src/caesarhu/project_euler/euler_071.clj
@@ -0,0 +1,21 @@
+(ns caesarhu.project-euler.euler-071)
+
+(defn euler-071
+  [target limit]
+  (when (ratio? target)
+    (let [n (numerator target)
+          d (denominator target)
+          target-ratio (fn [x]
+                         (let [xd (quot (* x n) d)]
+                           (if (= (* xd d) (* n x))
+                             (/ (dec xd) x)
+                             (/ xd x))))
+          start (max 2 (long (* d (dec (quot limit 7)))))]
+      (->> (range start (inc limit))
+           (map target-ratio)
+           (apply max)
+           numerator))))
+
+(comment
+  (time (euler-071 3/7 1000000))
+  )
