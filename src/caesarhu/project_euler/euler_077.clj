aeb84f1b13f96942835ae7acfb32901208eb2c62
caesarhu <hupeishun@gmail.com>
2022-02-07 16:54:24 +0800

    update euler-078

---
+++ src/caesarhu/project_euler/euler_077.clj
@@ -0,0 +1,24 @@
+(ns caesarhu.project-euler.euler-077
+  (:require [caesarhu.math.primes :as p]
+            [clojure.core.memoize :as m]))
+
+(defn count-prime-sums
+  [n prime-seq]
+  (cond
+    (or (neg? n) (empty? prime-seq)) 0
+    (zero? n) 1
+    :else (+ (count-prime-sums n (rest prime-seq))
+             (count-prime-sums (- n (first prime-seq)) prime-seq))))
+
+(defn prime-sums
+  [n]
+  (count-prime-sums n (p/primes-range n)))
+
+(defn euler-077
+  [limit]
+  (->> (iterate inc 1)
+       (some #(and (> (prime-sums %) limit) %))))
+
+(comment
+  (time (euler-077 5000))
+  )
---
+++ src/caesarhu/project_euler/euler_078.clj
@@ -0,0 +1,31 @@
+(ns caesarhu.project-euler.euler-078
+  (:require [clojure.core.memoize :as m]))
+
+(def mod-number 1000000)
+(def sign (cycle [1 -1]))
+
+(defn pentagon
+  [n]
+  (quot (- (* n n 3) n) 2))
+
+(def pentagon-algorithm
+  (memoize
+   (fn [n]
+     (cond
+       (= n 0) 1
+       (neg? n) 0
+       :else (letfn [(term [k] (+ (pentagon-algorithm (- n (pentagon k)))
+                                  (pentagon-algorithm (- n (pentagon (- k))))))]
+               (mod
+                (reduce + (take-while (comp not zero?)
+                                      (map #(* (term %2) %1) sign (iterate inc 1))))
+                mod-number))))))
+
+(defn euler-078
+  []
+  (->> (iterate inc 1)
+       (some #(and (zero? (pentagon-algorithm %)) %))))
+
+(comment
+  (time (euler-078))
+  )
