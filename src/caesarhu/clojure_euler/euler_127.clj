843ca5a3b112ffa5b73ccad0b5b8865f4a771c86
caesarhu <hupeishun@gmail.com>
2023-01-13 15:46:03 +0800

    update euler-127

--- src/caesarhu/clojure_euler/euler_127.clj
+++ src/caesarhu/clojure_euler/euler_127.clj
@@ -1,34 +1,39 @@
 (ns caesarhu.clojure-euler.euler-127
-  (:require [caesarhu.math.math-tools :refer [coprime?]] 
+  (:require [caesarhu.math.math-tools :refer [coprime?]]
+            [clojure.data.priority-map :refer [priority-map]]
             [caesarhu.math.primes :as p]
             [clojure.math.numeric-tower :refer [sqrt]]))
 
-(defn rad-range
+(defn rad-map
   [limit]
-  (let [rad (atom (vec (cons 0 (repeat (dec limit) 1))))]
+  (let [rad (atom (vec (repeat limit 1)))]
     (doseq [p (p/primes)
             :while (< p limit)
             px (iterate #(+ p %) p)
             :while (< px limit)]
       (swap! rad update px * p))
-    [@rad (reduce (fn [acc i] (update acc (@rad i) conj i))
-                  (vec (repeat limit (sorted-set))) (range limit))]))
+    (reduce (fn [acc i]
+              (conj acc [i (@rad i)]))
+            (priority-map) (range 1 limit))))
 
 (defn abc-hits
   [limit]
-  (let [[num-v rad-v] (rad-range limit)
+  (let [m (rad-map limit)
         valid-ab (fn [c x]
-                   (when-let [y (and (coprime? c x) (- c x))]
-                     (when (< (apply * (map num-v [x y c])) c)
-                       [x y])))
-        mul-c (fn [c]
-                (when-let [qc (as-> (/ c (num-v c)) q (when (> q 1) q))]
-                  (->> (mapcat (fn [i] (subseq (rad-v i) < c)) (range 1 (sqrt qc)))
-                       (mapcat (partial valid-ab c))
-                       (apply sorted-set)
-                       (#(subseq % < (/ c 2)))
-                       (map #(vector % (- c %) c)))))]
-    (mapcat mul-c (range 1 limit))))
+                   (let [y (- c x)]
+                     (if (and (coprime? c x) (< (apply * (map m [x y c])) c))
+                       [x y]
+                       [])))
+        valid-c (fn [c]
+                  (when-let [qc (as-> (/ c (m c)) q (when (> q 2) q))]
+                    (->> (subseq m < (sqrt qc))
+                         (map first)
+                         (filter #(< % c))
+                         (mapcat #(valid-ab c %))
+                         (apply sorted-set)
+                         (#(subseq % < (/ c 2)))
+                         (map #(vector % (- c %) c)))))]
+    (mapcat valid-c (range 1 limit))))
 
 (defn euler-127
   [limit]
