3c8e13078bdca4d226a4184d827c55d9f58d98e4
caesarhu <hupeishun@gmail.com>
2024-01-19 07:50:21 +0800

    euler-160

--- src/caesarhu/clojure_euler/euler_160.clj
+++ src/caesarhu/clojure_euler/euler_160.clj
@@ -1,53 +1,40 @@
 (ns caesarhu.clojure-euler.euler-160
-  (:require [caesarhu.math.math-tools :refer [power-mod factorial]]
+  (:require [caesarhu.math.math-tools :refer [power-mod digits]]
             [clojure.math.numeric-tower :refer [expt gcd]]
             [caesarhu.math.primes :as p]))
 
-(defn factorial-exp
+(defn legendre
   [n p]
-  (->> (map #(expt p %) (iterate inc 1))
-       (take-while #(<= % n))
-       (map #(quot n %))
-       (apply +)))
+  (quot (- n (apply + (digits n p)))
+        (dec p)))
 
-(defn modulo-without25
-  [modulo]
-  (reduce (fn [v n]
-            (mod (* v n) modulo))
-          1
-          (->> (iterate #(+ 2 %) 3)
-               (take-while #(< % modulo))
-               (remove #(zero? (mod % 5))))))
+(defn modulo*
+  [modulo & more]
+  (reduce (fn [v i]
+            (mod (* v i) modulo))
+          more))
 
-(defn g
-  [n modulo modulo-without25-modulo]
-  (let [n (mod n modulo)]
-    (reduce (fn [v n]
-              (mod (* v n) modulo))
-            modulo-without25-modulo
-            (->> (iterate #(+ 2 %) 3)
-                 (take-while #(< % (inc n)))
-                 (remove #(zero? (mod % 5)))))))
-
-(defn h
-  [n modulo modulo-without25-modulo]
-  (if (< n 2) 1
-      (mod (* (g n modulo modulo-without25-modulo) (h (quot n 5) modulo modulo-without25-modulo)) modulo)))
-
-(defn f
-  [n modulo modulo-without25-modulo]
-  (if (< n 2) 1
-      (mod (* (g n modulo modulo-without25-modulo) (f (quot n 2) modulo modulo-without25-modulo) (h (quot n 5) modulo modulo-without25-modulo)) modulo)))
+(defn modulo-other
+  [modulo n init]
+  (apply modulo* modulo init
+         (->> (range 1 (inc (mod n modulo)) 2) (remove #(zero? (mod % 5))))))
 
 (defn euler-160
   [n modulo]
-  (let [modulo-without25-modulo (power-mod (modulo-without25 modulo) (quot n modulo) modulo)
-        x (factorial-exp n 2)
-        y (factorial-exp n 5)
-        s (power-mod 2 (- x y) modulo)]
-    (mod (* s (f n modulo modulo-without25-modulo)) modulo)))
+  (let [init (apply modulo* modulo 1 (->> (range 1 (inc modulo) 2) (remove #(zero? (mod % 5)))))
+        e2 (- (legendre n 2) (legendre n 5))
+        e2r (power-mod 2 e2 modulo)]
+    (letfn [(f5 [n]
+                (if (< n 2) 1
+                    (modulo* modulo (modulo-other modulo n init) (f5 (quot n 5)))))
+            (f2 [n]
+                (if (< n 2) 1
+                    (modulo* modulo
+                             (modulo-other modulo n init)
+                             (f2 (quot n 2))
+                             (f5 (quot n 5)))))]
+      (modulo* modulo e2r (f2 n)))))
 
 (comment
-  (p/totient (expt 10 5))
   (time (euler-160 (expt 10 12) 100000))
   )
