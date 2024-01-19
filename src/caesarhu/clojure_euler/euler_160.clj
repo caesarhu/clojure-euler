94dd391e478a3e09be2d0a6bbabf66aa355fa48f
caesarhu <hupeishun@gmail.com>
2024-01-19 09:21:22 +0800

    euler-160

--- src/caesarhu/clojure_euler/euler_160.clj
+++ src/caesarhu/clojure_euler/euler_160.clj
@@ -8,32 +8,25 @@
   (quot (- n (apply + (digits n p)))
         (dec p)))
 
-(defn modulo*
-  [modulo & more]
-  (reduce (fn [v i]
-            (mod (* v i) modulo))
-          more))
-
-(defn modulo-other
-  [modulo n init]
-  (apply modulo* modulo init
-         (->> (range 1 (inc (mod n modulo)) 2) (remove #(zero? (mod % 5))))))
-
 (defn euler-160
   [n modulo]
-  (let [init (apply modulo* modulo 1 (->> (range 1 (inc modulo) 2) (remove #(zero? (mod % 5)))))
+  (let [mod* (fn [& more] (reduce #(mod (* %1 %2) modulo) more))
+        init (apply mod* 1 (->> (range 1 (inc modulo) 2) (remove #(zero? (mod % 5)))))
         e2 (- (legendre n 2) (legendre n 5))
-        e2r (power-mod 2 e2 modulo)]
-    (letfn [(f5 [n]
-                (if (< n 2) 1
-                    (modulo* modulo (modulo-other modulo n init) (f5 (quot n 5)))))
-            (f2 [n]
-                (if (< n 2) 1
-                    (modulo* modulo
-                             (modulo-other modulo n init)
-                             (f2 (quot n 2))
-                             (f5 (quot n 5)))))]
-      (modulo* modulo e2r (f2 n)))))
+        e2r (power-mod 2 e2 modulo)
+        mod-other (fn [n init]
+                    (apply mod* init (->> (range 1 (inc (mod n modulo)) 2) (remove #(zero? (mod % 5))))))
+        fx (fn [n p]
+             (loop [n n
+                    v 1]
+               (if (< n 2) v
+                   (recur (quot n p) (mod* v (mod-other n init))))))
+        f2 (fn [n]
+             (loop [n n
+                    v 1]
+               (if (< n 2) v
+                   (recur (quot n 2) (mod* v (mod-other n init) (fx (quot n 5) 5))))))]
+    (mod* e2r (f2 n))))
 
 (comment
   (time (euler-160 (expt 10 12) 100000))
