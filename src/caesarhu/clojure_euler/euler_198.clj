b170a538dbdbb6aee6421ae38a51e474927e1b61
caesarhu <hupeishun@gmail.com>
2024-02-27 06:40:05 +0800

    euler-198

--- src/caesarhu/clojure_euler/euler_198.clj
+++ src/caesarhu/clojure_euler/euler_198.clj
@@ -2,25 +2,19 @@
 
 (defn euler-198
   [N D]
-  (let [answer (- (quot N 2) (quot D 2))
-        stack (atom (list))
-        push (fn [x] (swap! stack #(cons x %)))
-        pop (fn [] (let [x (first @stack)]
-                     (swap! stack rest)
-                     x))
-        count-ambiguous (fn [q qx] (quot (- N (* 2 q qx)) (* 2 q q)))]
-    (push [[0 1] [1 D] [1 (dec D)]])
-    (loop [answer answer]
-      (if (empty? @stack)
-        answer
-        (let [[[pl ql] [p q] [pr qr]] (pop)
-              nl (max (count-ambiguous q ql) 0)
-              nr (if (> q D) (max (count-ambiguous q qr) 0) 0)]
-          (when (pos? nl)
-            (push [[pl ql] [(+ p pl) (+ q ql)] [p q]]))
-          (when (pos? nr)
-            (push [[p q] [(+ p pr) (+ q qr)] [pr qr]]))
-          (recur (+ answer nl nr)))))))
+  (let [count-ambiguous (fn [q qx]
+                          (max 0 (quot (- N (* 2 q qx)) (* 2 q q))))]
+    (loop [answer (- (quot N 2) (quot D 2))
+           stack (list [[0 1] [1 D] [1 (dec D)]])]
+      (if (not-empty stack)
+        (let [[[pl ql] [p q] [pr qr]] (first stack)
+              nl (count-ambiguous q ql)
+              nr (if (> q D) (count-ambiguous q qr) 0)]
+          (recur (+ answer nl nr)
+                 (cond->> (rest stack)
+                   (pos? nl) (cons [[pl ql] [(+ p pl) (+ q ql)] [p q]])
+                   (pos? nr) (cons [[p q] [(+ p pr) (+ q qr)] [pr qr]]))))
+        answer))))
 
 (comment
   (time (euler-198 100000000 100))
---
+++ src/caesarhu/clojure_euler/euler_200.clj
@@ -0,0 +1,6 @@
+(ns caesarhu.clojure-euler.euler-200
+  (:require [caesarhu.math.primes :as p]))
+
+(comment
+  (p/factors 1992008)
+  )
