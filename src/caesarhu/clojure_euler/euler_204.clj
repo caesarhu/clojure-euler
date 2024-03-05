700bd93c8195ee5d02658ee843987ac5a84eef54
caesarhu <hupeishun@gmail.com>
2024-03-05 13:20:24 +0800

    update euler-204

--- src/caesarhu/clojure_euler/euler_204.clj
+++ src/caesarhu/clojure_euler/euler_204.clj
@@ -29,7 +29,7 @@
   (time (euler-204-slow (expt 10 8) 5))
   )
 
-(def primes (atom (vec (p/primes 1000))))
+(def prime-vec (vec (p/primes 1000)))
 
 (defn log2
   [x]
@@ -37,7 +37,7 @@
 
 (defn count-hamming-numbers
   [x j]
-  (let [pj (@primes j)]
+  (let [pj (prime-vec j)]
     (cond
       (= 1 x) 1
       (zero? j) (log2 x)
@@ -45,11 +45,26 @@
       :else (+ (count-hamming-numbers x (dec j))
                (count-hamming-numbers (quot x pj) j)))))
 
-(defn euler-204
+(defn euler-204-recursive
   [limit n]
   (let [target (->> (p/primes (inc n)) count dec)]
     (count-hamming-numbers limit target)))
 
+(defn euler-204
+  [limit n]
+  (let [p-vec (vec (p/primes (inc n)))]
+    (loop [hamming-numbers 0
+           h-list [[limit (dec (count p-vec))]]]
+      (if (empty? h-list)
+        hamming-numbers
+        (let [[x j] (first h-list)
+              pj (p-vec j)]
+          (cond
+            (= 1 x) (recur (inc hamming-numbers) (rest h-list))
+            (zero? j) (recur (+ (log2 x) hamming-numbers) (rest h-list))
+            (< x pj) (recur hamming-numbers (cons [x (dec j)] (rest h-list)))
+            :else (recur hamming-numbers (into (rest h-list) [[(quot x pj) j] [x (dec j)]]))))))))
+
 (comment
-  (time (euler-204 (expt 10 9) 100))
+  (time (euler-204 (expt 10 18) 100))
   )
