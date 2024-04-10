e4a0fe6d3d35529f23126ffa1270a8b500db0bd9
caesarhu <hupeishun@gmail.com>
2024-04-10 08:04:31 +0800

    add euler-120

---
+++ src/caesarhu/clojure_euler/euler_220.clj
@@ -0,0 +1,34 @@
+(ns caesarhu.clojure-euler.euler-220
+  (:require [clojure.math.numeric-tower :refer [expt]]
+            [clojure.string :as str]))
+
+(def mp (atom {0 [0, 0] 1 [ 0, 1]}))
+
+(defn cal
+  [c p]
+  (let [[cx cy] c
+        [px py] p]
+    [(+ cx (- py) cy) (+ cy px (- cx))]))
+
+(defn find-mp
+  [n]
+  (if-let [x (@mp n)]
+    x
+    (let [pw (loop [pw 1]
+               (if (< pw n)
+                 (recur (* 2 pw))
+                 pw))
+          x1 (find-mp (quot pw 2))
+          x2 (find-mp (- pw n))]
+      (swap! mp assoc n (cal x1 x2))
+      (@mp n))))
+
+(defn euler-220
+  [target]
+  (reset! mp {0 [0, 0] 1 [0, 1]})
+  (->> (find-mp target)
+       (str/join ",")))
+
+(comment
+  (time (euler-220 (expt 10 12)))
+  )
